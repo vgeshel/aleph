94cadb0c6783352ecd0c5209d3b4199fc9002236
ztellman <ztellman@gmail.com>
2011-01-18 16:19:25 -0800

    close InputStreams after they've been written per Ring spec

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.1.4"
+(defproject aleph "0.1.4.1"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
